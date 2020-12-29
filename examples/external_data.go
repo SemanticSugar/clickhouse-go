7b3dd9c64041ef9475a8cba436a59c9503efce5b
Alvaro Tuso <alvarotuso@hotmail.com>
2020-12-29 01:31:27 +0000

    rename references

--- examples/external_data.go
+++ examples/external_data.go
@@ -4,11 +4,11 @@ import (
 	"database/sql"
 	"database/sql/driver"
 	"fmt"
-	"github.com/ClickHouse/clickhouse-go/lib/column"
+	"github.com/SemanticSugar/clickhouse-go/lib/column"
 	"log"
 	"time"
 
-	"github.com/ClickHouse/clickhouse-go"
+	"github.com/SemanticSugar/clickhouse-go"
 )
 
 func main() {
